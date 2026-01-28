88d2e1cbf6d440dd45501bc286f3e572d90d544e
K3v1n-N <kevnoorda@gmail.com>
2026-01-27 19:27:16 -0700

    lazy load

---
+++ week 04/lazyload.js
@@ -0,0 +1,7 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+// Get last modified date
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
