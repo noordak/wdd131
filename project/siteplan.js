4eae456262d8c59ed43c2a61fc0a9b4c54a757bc
K3v1n-N <kevnoorda@gmail.com>
2026-02-05 19:31:16 -0700

    siteplan-planning

---
+++ project/siteplan.js
@@ -0,0 +1,8 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+// Get last modified date
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModified}`;
+
